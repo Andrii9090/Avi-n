e71e38cbc0d1cfce104b76b5d00d12ee1aae7ec9
KOMPANNIETS  ANDRII <01190319@CEBANCDEA.EDU>
2021-04-23 21:23:08 +0200

    23/04

--- app/src/main/java/com/example/avion/MainActivity.java
+++ app/src/main/java/com/example/avion/MainActivity.java
@@ -5,35 +5,74 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner spinnerClases;
-    Spinner spinnerUbicaciones;
+    private Spinner spinnerClases;
+    private Spinner spinnerUbicaciones;
+    ArrayAdapter<CharSequence> spinnerUbicacionesAdapter;
     Avion avion;
+
+    Clase claseSelect;
+    Ubicacion ubicacionSelect;
+
+    EditText nombre;
+    EditText dni;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         spinnerClases = (Spinner) findViewById(R.id.spinner_clases);
-        ArrayAdapter<CharSequence> spinnerClasesAdapter = ArrayAdapter.createFromResource(this,R.array.array_clases, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> spinnerClasesAdapter = ArrayAdapter.createFromResource(this, R.array.array_clases, android.R.layout.simple_spinner_item);
         spinnerClasesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerClases.setAdapter(spinnerClasesAdapter);
 
         spinnerUbicaciones = (Spinner) findViewById(R.id.spinner_ubicaciones);
-        ArrayAdapter<CharSequence> spinnerUbicacionesAdapter = ArrayAdapter.createFromResource(this,R.array.array_ubicaciones, android.R.layout.simple_spinner_item);
+        spinnerUbicacionesAdapter = ArrayAdapter.createFromResource(this, R.array.array_ubicaciones, android.R.layout.simple_spinner_item);
         spinnerUbicacionesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerUbicaciones.setAdapter(spinnerUbicacionesAdapter);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
         avion = new Avion();
+
+        spinnerClases.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(id == 1){
+                    claseSelect = Clase.ECONOMICA;
+                    if(spinnerUbicacionesAdapter.getPosition("Centro")==-1) {
+                        spinnerUbicacionesAdapter.add("Centro");
+                    }
+                }
+                if(id == 2){
+                    claseSelect = Clase.EJECUTIVA;
+                    spinnerUbicacionesAdapter.remove("Centro");
+                }
+            }
+        });
     }
 
     public void reservar(View view) {
-        String clase = (String) spinnerClases.getSelectedItem();
+        String ubicacion = (String) spinnerUbicaciones.getSelectedItem();
+
+        if(ubicacion.equals("Ventana")){
+            ubicacionSelect = Ubicacion.VENTANA;
+        }
+        if(ubicacion.equals("Centro")){
+            ubicacionSelect = Ubicacion.VENTANA;
+        }
+        if(ubicacion.equals("Pasillo")){
+            ubicacionSelect = Ubicacion.VENTANA;
+        }
+        nombre = view.findViewById(R.id.nombre);
+        dni = view.findViewById(R.id.dni);
+        Pasajero pasajero = new Pasajero(nombre.getText().toString(), dni.getText().toString());
 
     }
 }
